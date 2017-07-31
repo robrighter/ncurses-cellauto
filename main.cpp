3a6e68940ca031ea4bfc825acdcb7d554c4af64c
Rob Righter <robrighter@gmail.com>
2017-07-30 23:23:41 -0400

    --

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 #include <stdio.h>
 #include <locale.h>
 #include <curses.h>
+#include "conway.h"
 
 #define BLOCK L'\uE030'
 int main(void) {
 
-    WINDOW * mainwin, * childwin;
+    WINDOW * mainwin;
     int ch = '0';
     int x = 0;
     int y = 0;
@@ -16,6 +17,12 @@ int main(void) {
     int width, height = 400;
     char mover = '@';
     char label[45];
+
+    Conway conway = Conway();
+    conway.seedTheCanvas();
+
+    bool getValueAtLocation(int x, int y);
+    void processNextCycle();
 
     setlocale(LC_ALL,"en_US.UTF-8");
     /*  Initialize ncurses  */
@@ -31,12 +38,22 @@ int main(void) {
     }
     start_color();
     init_pair(1, COLOR_RED, COLOR_WHITE);
+    init_pair(2, COLOR_MAGENTA, COLOR_CYAN);
+    attron(COLOR_PAIR(2));
+
+    printw(" Cellular Automaton - Conway's Game of Life"); 
+    getmaxyx(stdscr, rows, cols);
+    int spacing = cols - 43;
+    for(int i=0; i< spacing; i++){
+        printw(" ");
+    }
+    attroff(COLOR_PAIR(2));
+    mvaddch(1,0, ' ');
     attron(COLOR_PAIR(1));
     noecho();
     keypad(mainwin, TRUE);
     
 
-    printw("This is a test\nTo see if this even works"); 
     refresh();
     
     /*  Loop until user hits 'q' to quit  */
